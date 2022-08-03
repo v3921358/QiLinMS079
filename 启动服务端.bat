f8f8a42fe75d0fba78137cb4911ef66942aeb7bc
Windyboy <y765432100@hotmail.com>
2022-08-03 23:15:44 +0800

    修正啟動Bat

--- 启动服务端.bat
+++ 启动服务端.bat
@@ -1,10 +1,7 @@
 @echo off
-@title Êý¾Ý¿âMysql1.7
+@title CMS079
 Color 1c
-set path=config\bin;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%
-set JRE_HOME=config
-set JAVA_HOME=config
-set CLASSPATH=.;config\*;
+set CLASSPATH=.;lib\*;
 
 java -XX:-UseGCOverheadLimit -Xms512m -Xmx1024m -Xss128k -Dnet.sf.odinms.wzpath=wz gui.QILIN
 pause
