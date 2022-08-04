d861aba061b9c32c66db70db270705b581384dc4
Windyboy <y765432100@hotmail.com>
2022-08-04 19:48:18 +0800

    修正啟動bat

--- 启动服务端.bat
+++ 启动服务端.bat
@@ -3,5 +3,5 @@
 Color 1c
 set CLASSPATH=.;lib\*;
 
-java -XX:-UseGCOverheadLimit -Xms512m -Xmx1024m -Xss128k -Dnet.sf.odinms.wzpath=wz gui.QILIN
+java -XX:-UseGCOverheadLimit -Xms512m -Xmx1024m -Xss128k -Dnet.sf.odinms.wzpath=wz gui.Qhms
 pause
